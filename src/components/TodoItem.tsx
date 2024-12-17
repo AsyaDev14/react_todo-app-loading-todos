e74ac287a57d14a975dd8631291755611afb6640
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2024-12-17 15:17:54 +0200

    first part solution

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoItem: React.FC<Props> = props => {
+  const { todo } = props;
+  const [isCompleted] = useState<boolean>(todo.completed);
+
+  // const handleCompleted = () => {
+  //   setIsCompleted(!isCompleted);
+  // };
+
+  return (
+    <div data-cy="Todo" className={`todo ${isCompleted && 'completed'}`}>
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={isCompleted}
+          // onChange={handleCompleted}
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo.title}
+      </span>
+
+      {/* Remove button appears only on hover */}
+      <button type="button" className="todo__remove" data-cy="TodoDelete">
+        ×
+      </button>
+
+      {/* overlay will cover the todo while it is being deleted or updated */}
+      <div data-cy="TodoLoader" className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
